36dd4f24d03c5a5e0efd3ff9d415e107d3dc2cd0
airj23 <airj23@example.com>
2021-11-01 23:29:36 +0530

    Global Descriptor Table Added

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,10 +1,30 @@
+#include "types.h"
+#include "gdt.h"
 
-
-
-
-void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+void printf(char* str)
 {
-	printf("Hello World");
-	while (1);
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
+    for(int i = 0; str[i] != '\0'; ++i)
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+        (*i)();
+}
+
+
+
+extern "C" void kernelMain(const void* multiboot_structure, unsigned int /*multiboot_magic*/)
+{
+    printf("Hello World from Jai Arora!");
+    GlobalDescriptorTable gdt;
+    while(1);
 }
